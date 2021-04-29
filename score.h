115b1c2419215e77e4e23a5f28f090a43a44a7cf
2000567 <e2000567@shell.vamk.fi>
2021-04-29 16:26:42 +0300

    Added the third and final modification to score.h

--- score.h
+++ score.h
@@ -1,9 +1,5 @@
 #pragma once
 #define _CRT_SECURE_NO_WARNINGS
-// What to write in a header file:
-// 1. Constant definition (e.g., #define PI 3.1415)
-// 2. Data structure definitions
-// 3. Function prototypes (aka. function declaration)
 #include <stdio.h>
 #define MAX 30
 
